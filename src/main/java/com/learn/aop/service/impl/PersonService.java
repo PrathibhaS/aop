c01d6895cc803d3cb97e229cfdc24a0e74805b0b
Prathibha <prathibha1603@gmail.com>
2020-09-02 00:17:14 +0100

    Add annotation

--- src/main/java/com/learn/aop/service/impl/PersonService.java
+++ src/main/java/com/learn/aop/service/impl/PersonService.java
@@ -4,9 +4,11 @@ import com.learn.aop.model.Person;
 import com.learn.aop.repository.IPersonRepository;
 import com.learn.aop.service.IPersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class PersonService implements IPersonService {
 
     @Autowired
